a8a0983776f2ba1728d496dfa860777c100346e1
bcl <cohen.lhyver@gmail.com>
2016-06-27 15:07:31 +0200

    Class added

--- head_turning_modulation_model_trudata/Sources/OdiInterface.m
+++ head_turning_modulation_model_trudata/Sources/OdiInterface.m
@@ -1,4 +1,4 @@
-classdef JidoInterface < handle
+classdef OdiInterface < handle
     %JIDOINTERFACE Summary of this class goes here
     %   Detailed explanation goes here
     
@@ -11,7 +11,8 @@ classdef JidoInterface < handle
                                 % KEMAR head in degrees.
         AzimuthMin              % Minimum azimuthal look direction of the
                                 % KEMAR head in degrees.
-        LengthOfSimulation      
+        LengthOfSimulation      % TODO: How to integrate this?
+        
     end
     
     properties (Access = public)
@@ -19,68 +20,85 @@ classdef JidoInterface < handle
         kemar                   % KEMAR control interface.
         jido                    % Jido interface.
         bass                    % Interface to the audio stream server.
+        qr_vision
+        finished  %% added to stop bbs.run()
     end
     
     methods (Access = public)
-        function obj = JidoInterface(pathToGenomix)
-            % JIDOINTERFACE Constructor...
-            
-            % Check if path to genomix is valid
-            if ~exist(pathToGenomix, 'dir')
-                error('Wrong path to genomix.');
-            end
-            
-            % Add path to genomix
-            userpath(pathToGenomix);
+        function obj = OdiInterface()
+            pathToGenomix = '~/openrobots/lib/matlab';
+            addpath(genpath(pathToGenomix));
             
             % Set up genomix client
-            obj.client = genomix.client('jido-base:8080');
+            odi_base = '192.168.11.100:8080';
+            obj.client = genomix.client(odi_base);
+            %obj.client = genomix.client('jido-base:8080');
             
             % Load KEMAR module
-            obj.kemar = obj.client.load('kemar');
+            % obj.kemar = obj.client.load('kemar');
             
             % Load JIDO module
-            obj.jido = obj.client.load('sendPosition');
+            obj.jido = obj.client.load('sendPosition'); % Il faut lier les ports
             
             % Load BASS module
             obj.bass = obj.client.load('bass');
             
+            %obj.qr_vision = obj.client.load('QR2matlab');
+            
+                currentPositionPort = obj.jido.connect_port('currentPosition', 'currentPosition');
+                goalStatusArray = obj.jido.connect_port('GoalStatus', 'move_base/status');
+%                 obj.qr_vision.connect_port('messageIn', '/visp_auto_tracker/code_message' );
+%                 obj.qr_vision.connect_port('poseIn', '/visp_auto_tracker/object_position' );
+               
+            
+            hardware = 'hw:2,0' ;
+            obj.SampleRate = 44100 ;
+            nFramesPerChunk = 2205 ;
+            nChunksOnPort = 20*0.5 ;
+            obj.bass.Acquire('-a', hardware, obj.SampleRate, nFramesPerChunk, nChunksOnPort) ;
+            
+%             QR2 = obj.qr_vision.Publish('-a');
+            
             % Get BASS status info
-            audioObj = obj.bass.Audio();
-            obj.SampleRate = audioObj.Audio.sampleRate;
-            obj.BlockSize = audioObj.Audio.nFramesPerChunk * ...
-                audioObj.Audio.nChunksOnPort;
+            % audioObj = obj.bass.Audio(); % A voir
+            obj.BlockSize = nFramesPerChunk * ...
+                nChunksOnPort;
             
             % Get KEMAR properties
-            kemarState = obj.kemar.currentState();
-            obj.AzimuthMax = kemarState.currentState.maxLeft;
-            obj.AzimuthMin = kemarState.currentState.maxRight;
+            % kemarState = obj.kemar.currentState();
+            % obj.AzimuthMax = kemarState.currentState.maxLeft;
+            % obj.AzimuthMin = kemarState.currentState.maxRight;
+            obj.finished = false %% init a false
         end
         
-        function configureAudioStreamServer(obj, sampleRate, frameSize, ...
-                bufferSizeSec)
-            % CONFIGUREAUDIOSTREAMSERVER
-            
-            % Check input arguments
-            p = inputParser();
-            
-            p.addRequired('SampleRate', @(x) validateattributes(x, ...
-                {'numeric'}, {'scalar', 'real'}));
-            p.addRequired('FrameSize', @(x) validateattributes(x, ...
-                {'numeric'}, {'scalar', 'integer'}));
-            p.addRequired('BufferSizeSec', @(x) validateattributes(x, ...
-                {'numeric'}, {'scalar', 'real'}));
-            p.parse(sampleRate, frameSize, bufferSizeSec);
-            
-            % Compute number of frames per chunk
-            numFramesPerChunk = ceil(p.Results.BufferSizeSec * ...
-                p.Results.SampleRate / p.Results.FrameSize);
-            
-            % Setup audio stream server
-            obj.bass.Acquire('-a', 'hw:1,0', p.Results.SampleRate, ...
-                p.Results.FrameSize, numFramesPerChunk);            
-        end
-        
+%         function configureAudioStreamServer(obj, sampleRate, frameSize, ...
+%                 bufferSizeSec)
+%             % CONFIGUREAUDIOSTREAMSERVER
+%             
+%             % Check input arguments
+%             p = inputParser();
+%             
+%             p.addRequired('SampleRate', @(x) validateattributes(x, ...
+%                 {'numeric'}, {'scalar', 'real'}));
+%             p.addRequired('FrameSize', @(x) validateattributes(x, ...
+%                 {'numeric'}, {'scalar', 'integer'}));
+%             p.addRequired('BufferSizeSec', @(x) validateattributes(x, ...
+%                 {'numeric'}, {'scalar', 'real'}));
+%             p.parse(sampleRate, frameSize, bufferSizeSec);
+%             
+%             % Compute number of frames per chunk
+%             numFramesPerChunk = ceil(p.Results.BufferSizeSec * ...
+%                 p.Results.SampleRate / p.Results.FrameSize);
+%             
+%             % Setup audio stream server
+%             obj.bass.Acquire('-a', 'hw:1,0', p.Results.SampleRate, ...
+%                 p.Results.FrameSize, numFramesPerChunk);
+%             
+%             % Update block size
+%             obj.BlockSize = round(numFramesPerChunk * p.Results.FrameSize);
+%         end
+
+
         function [earSignals, signalLengthSec] = getSignal(obj, ...
                 timeDurationSec)
             % GETSIGNAL This function acquires binaural signals from the
@@ -91,8 +109,8 @@ classdef JidoInterface < handle
             
             % Get binaural signals
             % Sclaing factor estimated empirically
-            earSignals = [cell2mat(audioBuffer.Audio.left) ./ (2^31); ...
-                0.7612 .* (cell2mat(audioBuffer.Audio.right) ./ (2^31))]';
+            earSignals = [cell2mat(audioBuffer.Audio.left)' ./ (2^31), ...
+                0.7612 .* (cell2mat(audioBuffer.Audio.right)' ./ (2^31))];
             
             % Get default buffer size of the audio stream server
             bufferSize = size(earSignals, 1);
@@ -109,13 +127,21 @@ classdef JidoInterface < handle
                 error(['Desired chunk length exceeds length of the ', ...
                     'audio buffer.']);
             end
-            
+
             % Get corresponding signal chunk
             earSignals = earSignals(bufferSize - chunkLength + 1 : end, :);
             
             % Get signal length
             signalLengthSec = length(earSignals) / ...
                 audioBuffer.Audio.sampleRate;
+            
+        end
+        
+        function output = getQR (obj)
+            
+            output = obj.qr_vision.dataOut();
+            output = output.dataOut().message;
+            
         end
         
         function moveRobot(obj, posX, posY, theta, varargin)
@@ -139,10 +165,10 @@ classdef JidoInterface < handle
             % Execute motion command depending on selected mode
             switch p.Results.Mode
                 case 'absolute'
-                    obj.jido.moveAbsolutePosition('/map', ...
+                    obj.jido.moveAbsolutePosition('-a', '/map', ...
                         p.Results.PosX, p.Results.PosY, p.Results.Theta);
                 case 'relative'
-                    obj.jido.moveRelativePosition('/map', ...
+                    obj.jido.moveRelativePosition('-a', '/map', ...
                         p.Results.PosX, p.Results.PosY, p.Results.Theta);
                 otherwise
                     error('Mode not supported.');
@@ -163,6 +189,8 @@ classdef JidoInterface < handle
                 @(x) any(validatestring(x, expectedModes)));
             p.parse(angle, varargin{:});
             
+            % TODO: Prevent head from exceeding azimuth limits
+            
             % Execute motion command depending on selected mode
             switch p.Results.Mode
                 case 'absolute'
@@ -178,10 +206,15 @@ classdef JidoInterface < handle
             % GETCURRENTHEADORIENTATION
             
             % Get current state of the KEMAR head
-            kemarState = obj.kemar.currentState();
+%             kemarState = obj.kemar.currentState();
             
             % Get head angle
-            azimuth = kemarState.currentState.position;
+%             azimuth = kemarState.currentState.position;
+            % --- EDIT --- %
+            navig = obj.jido.NavigationState();
+            pos = navig.NavigationState.position;
+            azimuth = pos.orientation;
+            % --- EDIT --- %
         end
         
         function delete(obj)
@@ -206,7 +239,8 @@ classdef JidoInterface < handle
         
         function result = isFinished(obj)
             % Just live with this...
-            result = false;
+            %result = false;
+            result = obj.finished;
         end
     end
 end
