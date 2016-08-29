bed97df3b8b42fad44269c6369b825ea5444b416
bcl <cohen.lhyver@gmail.com>
2016-08-29 12:11:35 +0200

    Integration of the HTMFocusKS

--- head_turning_modulation_model_simdata/KnowledgeSources/MotorOrderKS.m
+++ head_turning_modulation_model_simdata/KnowledgeSources/MotorOrderKS.m
@@ -7,7 +7,7 @@ properties (SetAccess = public, GetAccess = public)
     head_position = 0;
     head_position_hist = [];
     htm;
-
+    HTMFocusKS;
 end
 % ======================== %
 % === PROPERTIES [END] === %
@@ -21,22 +21,26 @@ methods
 % ========================= %
 % === CONSTRUCTOR [BEG] === %
 % ========================= %
-function obj = MotorOrderKS (htm)
+function obj = MotorOrderKS (htm, htmFocusKS)
 	% obj.RIR = htm.RIR;
 	obj.htm = htm;
+    obj.HTMFocusKS = htmFocusKS;
 end
+% ========================= %
+% === CONSTRUCTOR [END] === %
+% ========================= %
 
 function moveHead (obj)
 	RIR = obj.htm.RIR;
     % --- If no sound -> make the head turn to 0° (resting state)
     % focus = RIR.focus;
-    focus = htm.HTMFocusKS.focus;
+    focus = obj.HTMFocusKS.focused_object;
 
     if obj.isFocusedObjectPresent() && focus ~= 0 % --- move the head
         % theta = obj.motorOrder();
         theta = getObject(RIR, RIR.focus, 'theta');
         % obj.RIR.updateAngle(0);
-        setObject(RIR, 0, 'theta', 0);
+        setObject(RIR, 0, 'theta', 0);            % --- go back to resting position (O°)
         %RIR.getLastObj().theta = 0;
     else                                          % --- go back to resting state (O°)
         theta = -obj.head_position;
@@ -47,15 +51,24 @@ end
 
 function bool = isFocusedObjectPresent (obj)
 	RIR = obj.htm.RIR;
+    focus = obj.HTMFocusKS.focused_object;
+
     if isempty(RIR.getEnv().present_objects)
-        bool = false ;
-    elseif find(RIR.focus == RIR.getEnv().present_objects)
-        bool = true ;
+        bool = false;
+    % elseif find(RIR.focus == RIR.getEnv().present_objects)
+    elseif find(focus == RIR.getEnv().present_objects)
+        bool = true;
     else
-        bool = false ;
+        bool = false;
     end
 end
 
 end
+% ===================== %
+% === METHODS [END] === %
+% ===================== %
 
 end
+% =================== %
+% === END OF FILE === %
+% =================== %
