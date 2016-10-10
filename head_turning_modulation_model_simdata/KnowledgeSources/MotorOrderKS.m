d211de72d90734ecb4066db5b8efedc47745a1ff
bcl <cohen.lhyver@gmail.com>
2016-10-10 09:54:26 +0200

    Code cleared

--- head_turning_modulation_model_simdata/KnowledgeSources/MotorOrderKS.m
+++ head_turning_modulation_model_simdata/KnowledgeSources/MotorOrderKS.m
@@ -7,7 +7,10 @@ properties (SetAccess = public, GetAccess = public)
     head_position = 0;
     head_position_hist = [];
     htm;
+    RIR;
     HTMFocusKS;
+
+    shm = 0;
 end
 % ======================== %
 % === PROPERTIES [END] === %
@@ -18,48 +21,57 @@ end
 % ===================== %
 methods
 
-% ========================= %
 % === CONSTRUCTOR [BEG] === %
-% ========================= %
 function obj = MotorOrderKS (htm, htmFocusKS)
-	% obj.RIR = htm.RIR;
 	obj.htm = htm;
+    obj.RIR = htm.RIR;
     obj.HTMFocusKS = htmFocusKS;
 end
-% ========================= %
 % === CONSTRUCTOR [END] === %
-% ========================= %
 
 function moveHead (obj)
-	RIR = obj.htm.RIR;
     % --- If no sound -> make the head turn to 0° (resting state)
-    % focus = RIR.focus;
     focus = obj.HTMFocusKS.focused_object;
-
-    if obj.isFocusedObjectPresent() && focus ~= 0 % --- move the head
-        % theta = obj.motorOrder();
-        theta = getObject(RIR, focus, 'theta');
-        % obj.RIR.updateAngle(0);
-        setObject(RIR, 0, 'theta', 0);            % --- go back to resting position (O°)
-        %RIR.getLastObj().theta = 0;
-    else                                          % --- go back to resting state (O°)
+    if focus == 0
         theta = -obj.head_position;
+
+    % --- Line above to be deleted -> already handle in the MotorOrderKS
+    elseif obj.isFocusedObjectPresent() %&& focus ~= 0   % --- move the head to 'theta'
+        theta = getObject(obj.RIR, focus, 'theta');
+        % --- TO BE PLACED IN OBJECTDETECTIONKS
+        % --- It's not up to this KS to set a new property to the object (-> AUDIOLOCALIZATIONKS)
+        setObject(obj.RIR, focus, 'theta', 0);
+    else                                            % --- go back to resting position (O°)
+        theta = -obj.head_position;
+        original_theta = getObject(obj.htm, focus, 'theta_hist');
+        setObject(obj.RIR, focus, 'theta', original_theta(1));
+        obj.RIR.getEnv().objects{focus}.theta_hist(end+1) = original_theta(1);
     end
     obj.head_position = mod(theta+obj.head_position, 360) ;
     obj.head_position_hist(end+1) = obj.head_position;
+
+    obj.RIR.head_position = obj.head_position;
+
+    obj.computeSHM();
 end
 
 function bool = isFocusedObjectPresent (obj)
-	RIR = obj.htm.RIR;
     focus = obj.HTMFocusKS.focused_object;
 
-    if isempty(RIR.getEnv().present_objects)
+    if obj.RIR.nb_objects == 0
         bool = false;
-    % elseif find(RIR.focus == RIR.getEnv().present_objects)
-    elseif find(focus == RIR.getEnv().present_objects)
+    elseif getObject(obj.RIR, focus, 'presence')
         bool = true;
     else
         bool = false;
+    end
+end
+
+function computeSHM (obj)
+    if numel(obj.head_position_hist) > 1
+        if obj.head_position_hist(end-1) ~= obj.head_position_hist(end)
+            obj.shm = obj.shm+1;
+        end
     end
 end
 
@@ -67,7 +79,6 @@ end
 % ===================== %
 % === METHODS [END] === %
 % ===================== %
-
 end
 % =================== %
 % === END OF FILE === %
