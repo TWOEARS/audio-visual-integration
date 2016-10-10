10b082ef5d64f3971df4008c764539a4d092fa0d
bcl <cohen.lhyver@gmail.com>
2016-10-10 09:52:30 +0200

    New function
    Related to the creation of the environment.

---
+++ head_turning_modulation_model_simdata/Tools/assignSource.m
@@ -0,0 +1,23 @@
+function repartition = assignSource (scene, nb_sources)
+
+	repartition = cell(1, numel(scene));
+	pairs = randperm(numel(scene));
+	sources = randperm(nb_sources);
+
+	nb_remaining_sources = nb_sources;
+	nb_remaining_pairs = numel(scene);
+	
+	for iPair = pairs(1:end-1)
+
+		nb_assigned_sources = randi(nb_remaining_sources-nb_remaining_pairs);
+		idx_sources = randi(nb_remaining_sources, 1, nb_assigned_sources);
+		repartition{iPair} = sources(idx_sources);
+		sources(idx_sources) = [];
+		nb_remaining_sources = nb_remaining_sources - nb_assigned_sources;
+		nb_remaining_pairs = nb_remaining_pairs-1;
+
+	end
+
+	repartition{pairs(end)} = sources;
+
+end
