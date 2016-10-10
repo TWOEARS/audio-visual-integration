8d331ec71bf206d707d0c55945e915ac92aa7f11
bcl <cohen.lhyver@gmail.com>
2016-10-10 09:52:16 +0200

    Minor changes
    Sources position known in advance (for the simulated environment) ->
    taken into account in the creation of the environment.

--- head_turning_modulation_model_simdata/Tools/generateAngle.m
+++ head_turning_modulation_model_simdata/Tools/generateAngle.m
@@ -1,5 +1,9 @@
-function theta = generateAngle ()
+function theta = generateAngle (label)
     % theta = randi(360) ;
-    angles = getInfo('sources_position');
-    theta = angles(randi(getInfo('nb_angles')));
+    % angles = getInfo('sources_position');
+    info = getInfo('all');
+    avpairs = mergeLabels(info.AVPairs(info.scenario.scene{1}));
+    tmp = strcmp(label, avpairs);
+    theta = info.sources_position(tmp);
+    % theta = angles(randi(getInfo('nb_angles')));
 end
