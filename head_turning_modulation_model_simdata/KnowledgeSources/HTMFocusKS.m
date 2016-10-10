1686f290ed4f3654a5c93d22b3e63863470fa97e
bcl <cohen.lhyver@gmail.com>
2016-10-10 09:54:33 +0200

    Code cleared

--- head_turning_modulation_model_simdata/KnowledgeSources/HTMFocusKS.m
+++ head_turning_modulation_model_simdata/KnowledgeSources/HTMFocusKS.m
@@ -24,7 +24,9 @@ properties (SetAccess = public)
     focused_object = 0;
     focus_origin = 0; % to be renamed as "focus_type"
     previous_focus = 0;
-    focus_hist = [];
+    focus_hist = 0;
+
+    shm = 0;
 
 end
 
@@ -44,7 +46,7 @@ end
 function computeFocus (obj)
     RIR = obj.RIR; % --- RobotInternalRepresentaion
 
-    if isempty(RIR.getEnv().objects)
+    if RIR.nb_objects == 0
         obj.focus_hist = [obj.focus_hist, 0];
         obj.focus_origin = [obj.focus_origin, 0];
         return;
@@ -57,11 +59,15 @@ function computeFocus (obj)
     mfi_focus = obj.computeMFIFocus();
 
     % --- Comparison of the two results
-    if mfi_focus == 0
-        focus = dwmod_focus ;
+    if mfi_focus == 0 && dwmod_focus ~= 0 % DWmod takes the lead
+        focus = dwmod_focus;
         focus_origin = 1;
-    else
-        focus = mfi_focus ;
+    elseif mfi_focus == 0 && dwmod_focus == 0 % No focused object
+        focus = obj.focus_hist(end);
+        % focus = 0;
+        focus_origin = 0;
+    else % MFImod takes the lead over the DWmod
+        focus = mfi_focus;
         focus_origin = -1;
     end
     % focus = mfi_focus ;
@@ -70,36 +76,68 @@ function computeFocus (obj)
         focus = 0;
     end
 
+    % obj.previous_focus = obj.focused_object;
     obj.focused_object = focus;
-    obj.focus_origin = [obj.focus_origin, focus_origin];
+    obj.focus_origin(end+1) = focus_origin;
+    obj.focus_hist(end+1) = obj.focused_object;
 
-    obj.focus_hist = [obj.focus_hist, obj.focused_object];
+    obj.computeSHM();
 
 end
 
 % === Compute focused object thanks to the DYNAMIC WEIGHTING module (DWmod) algorithm
 function focus = computeDWmodFocus (obj)
     focus = obj.getMaxWeightObject();
-    if getObject(obj.RIR, focus, 'weight') < 0.98
+    object = getObject(obj.RIR, focus);
+    if object.weight < 0.98
+        focus = 0;
+    elseif ~obj.isPerformant(object.cat)
         focus = 0;
     end
+        
 end
 
 % === Compute focused object thanks to the MULTIMODAL FUSION and INFERENCE module (MFImod) algorithm
 function focus = computeMFIFocus (obj)
     focus = 0;
-    if getObject(obj.RIR, 0, 'presence')
-        request = getObject(obj.RIR, 0, 'requests');
-        if request.check
-            focus = numel(obj.RIR.getEnv().objects);
+    current_object = obj.htm.current_object;
+    % current_object = obj.focused_object;
+    if current_object == 0
+        focus = 0;
+        return;
+    end
+
+    if getObject(obj.RIR, current_object, 'presence')
+        request = getObject(obj.RIR, current_object, 'requests');
+        if request.check 
+            % focus = numel(obj.RIR.getEnv().objects);
+            focus = current_object;
+            obj.RIR.getEnv().objects{current_object}.requests.checked = true;
+        elseif request.checked
+            % focus = numel(obj.RIR.getEnv().objects);
+            focus = current_object;
         end
     end
 end
 
+function bool = isPerformant (obj, idx)
+    env = obj.RIR.getEnv();
+    perf = cell2mat(getCategory(env, idx, 'perf'));
+    if perf >= getInfo('q') && perf < 1
+    % if obj.observed_categories{idx}.perf >= getInfo('q') && obj.observed_categories{idx}.perf < 1
+        bool = true;
+        % if obj.observed_categories{idx}.perf == 1 && obj.observed_categories{idx}.nb_inf < 7
+        %   bool = false;
+        % end
+    else
+        bool = false;
+    end
+end
+
 function computeSHM (obj)
-    if obj.focused_object ~= obj.previous_focus
+    if obj.focused_object ~= obj.previous_focus && obj.focused_object ~= 0
         obj.shm = obj.shm + 1;
-        obj.previous_focus = obj.focus;
+        obj.previous_focus = obj.focused_object;
     end
 end
 
