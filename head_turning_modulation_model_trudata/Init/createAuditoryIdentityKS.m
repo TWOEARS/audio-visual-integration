7c6bb02c1183039d7e285276e844416cab747299
bcl <cohen.lhyver@gmail.com>
2016-10-19 19:09:28 +0200

    Paths issues...

--- head_turning_modulation_model_trudata/Init/createAuditoryIdentityKS.m
+++ head_turning_modulation_model_trudata/Init/createAuditoryIdentityKS.m
@@ -1,7 +1,7 @@
 function KS = createAuditoryIdentityKS (bbs, models)
 
 	% folder = '/home/twoears/AuditoryModel/TwoEars-1.2/examples/identification_jido/Training.2016.01.31.21.11.16.074';
-	folder = '/home/twoears/AuditoryModel/TwoEars-1.2/twoears-demos/identification_distractor_NIGENS/models.0db';
+	folder = '/home/twoears/AuditoryModel/TwoEars-1.2/twoears-demos/identification_distractor_NIGENS/models.mc';
 	KS = arrayfun(@(x) bbs.createKS('IdentityKS', {models{x}, folder}),...
 				  1:numel(models),...
 				  'UniformOutput', false);
