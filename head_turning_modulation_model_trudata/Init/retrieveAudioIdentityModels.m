4d73e3813b77d381492d675506cb8525b79e12e5
bcl <cohen.lhyver@gmail.com>
2016-10-20 10:05:32 +0200

    Minor change

--- head_turning_modulation_model_trudata/Init/retrieveAudioIdentityModels.m
+++ head_turning_modulation_model_trudata/Init/retrieveAudioIdentityModels.m
@@ -5,9 +5,11 @@ function audio_labels = retrieveAudioIdentityModels (htm)
     idKSs = find(arrayfun(@(x) isa(KSs{x}, 'IdentityKS'), 1:numel(KSs)));
     
     modelnames = arrayfun(@(x) KSs{x}.modelname, idKSs, 'UniformOutput', false);
+
+    audio_labels = modelnames;
     
-    names = arrayfun(@(x) strfind(modelnames{x}, '.model.mat'), 1:numel(modelnames));
+    % names = arrayfun(@(x) strfind(modelnames{x}, '.model.mat'), 1:numel(modelnames));
     
-    audio_labels = arrayfun(@(x) modelnames{x}(1:names(x)-1), 1:numel(modelnames), 'UniformOutput', false);
+    % audio_labels = arrayfun(@(x) modelnames{x}(1:names(x)-1), 1:numel(modelnames), 'UniformOutput', false);
 
 end
