9e085cdab5339d88f6f6206547c611b04480783b
bcl <cohen.lhyver@gmail.com>
2016-11-18 13:13:57 +0100

    MC

--- head_turning_modulation_model_trudata/Init/createAuditoryIdentityKS.m
+++ head_turning_modulation_model_trudata/Init/createAuditoryIdentityKS.m
@@ -1,7 +1,9 @@
 function KS = createAuditoryIdentityKS (bbs, models)
 
 	% folder = '/home/twoears/AuditoryModel/TwoEars-1.2/examples/identification_jido/Training.2016.01.31.21.11.16.074';
-	folder = '/home/twoears/AuditoryModel/TwoEars-1.2/audio-visual-integration/head_turning_modulation_model_trudata/Dataset/mc2_models_dataset_1';
+	% folder = '/home/twoears/AuditoryModel/TwoEars-1.2/audio-visual-integration/head_turning_modulation_model_trudata/Dataset/mc2_models_dataset_1';
+
+    folder = '../../database-internal/learned_models/IdentityKS/mc3_fc3_segmented_nsGroundtruth_models_dataset_1';
 	% folder = '/home/twoears/AuditoryModel/TwoEars-1.2/twoears-demos/identification_distractor_NIGENS/models.0db';
 	KS = arrayfun(@(x) bbs.createKS('IdentityKS', {models{x}, folder}),...
 				  1:numel(models),...
