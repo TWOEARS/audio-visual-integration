eb6c6faea3f5e774c5d14883f3b5a445f40d709e
bcl <cohen.lhyver@gmail.com>
2016-11-21 00:52:35 +0100

    Improve speed of execution

--- head_turning_modulation_model_simdata/Tools/isPerformant.m
+++ head_turning_modulation_model_simdata/Tools/isPerformant.m
@@ -1,15 +1,23 @@
-function bool = isPerformant (obj, idx)
+function bool = isPerformant (obj, idx, varargin)
+% isPerformant function
+% This knowledge source aims at providing a representation of the internal representation of the environment
+% Author: Benjamin Cohen-Lhyver
+% Date: 26.09.16
+% Rev. 1.0
 
-	% if idx == 0
-	% 	bool = false;
-	% 	return;
-	% end
+	if isempty(idx)
+		bool = false;
+		return;
+	end
+    if nargin == 3 && strcmp(varargin{1}, 'Object')
+         idx = getObject(obj, idx, 'audiovisual_category');
+     end
 	
-    perf = cell2mat(getCategory(obj, idx, 'perf'));
+    perf = getCategory(obj, idx, 'perf');
     
     if perf >= getInfo('q') && perf < 1
         bool = true;
-    elseif perf == 1 && cell2mat(getCategory(obj, idx, 'nb_inf')) > 5
+    elseif perf == 1 && getCategory(obj, idx, 'nb_inf') >= 5
     	bool = true;
     else
         bool = false;
