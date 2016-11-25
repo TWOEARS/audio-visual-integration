49ece13311d1e9694e385aae7919a9b7c8c98363
bcl <cohen.lhyver@gmail.com>
2016-11-25 17:52:16 +0100

    MC

--- head_turning_modulation_model_trudata/KnowledgeSources/MotorOrderKS.m
+++ head_turning_modulation_model_trudata/KnowledgeSources/MotorOrderKS.m
@@ -80,7 +80,7 @@ function execute (obj)
 
     obj.computeSHM();
 
-    obj.robot.rotateHead(theta, 'relative');
+    % obj.robot.rotateHead(theta, 'relative');
 
     disp(['motorOrder: ', num2str(theta)]);
     disp(['headOrientation: ', num2str(currentHeadOrientation)]);
