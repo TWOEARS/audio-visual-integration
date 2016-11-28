ee9067000922db568543cd5cca8b3488349e07f0
bcl <cohen.lhyver@gmail.com>
2016-11-28 17:54:02 +0100

    MC

--- head_turning_modulation_model_trudata/Get/getClassifiersOutput.m
+++ head_turning_modulation_model_trudata/Get/getClassifiersOutput.m
@@ -3,22 +3,12 @@ function data = getClassifiersOutput (obj)
     audio_vec = getAudioClassifiersOutput(obj.blackboard);
 
     data = obj.blackboard.getLastData('visualIdentityHypotheses').data;
+    
+    present_objects = obj.blackboard.getLastData('visualStreamsHypotheses').data;
+    present_objects = present_objects('present_objects');
+
     visual_vec = zeros(getInfo('nb_visual_labels'), 1);
 
-function audio_vec = getAudioClassifiersOutput (blackboard)
-    audio_hyp = blackboard.getLastData('identityHypotheses') ;
-    audio_vec = cell2mat(arrayfun(@(x) audio_hyp.data(x).p,...
-                                  1:getInfo('nb_audio_labels'),...
-                                  'UniformOutput', false))' ;
-    audio_vec = audio_vec/sum(audio_vec) ;
-    audio_vec(isnan(audio_vec)) = 0 ;
-end
+    visual_vec(present_objects) = data('visual_labels');
 
-    % if isInFieldOfView(obj)
-    %     visual_vec = getVisualClassifiersOutput(obj);
-    % else
-    %     visual_vec = zeros(getInfo('nb_visual_labels'), 1);
-    % end
-
-    %request = [audio_vec ; visual_vec];
 end
