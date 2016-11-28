c2c4df750d8778e3e6b21122d776f60be82a48cb
bcl <cohen.lhyver@gmail.com>
2016-11-28 18:28:57 +0100

    MC

--- head_turning_modulation_model_trudata/Tools/getCategory.m
+++ head_turning_modulation_model_trudata/Tools/getCategory.m
@@ -1,23 +1,31 @@
-function request = getCategory (obj, idx, field)
+function request = getCategory (obj, idx, field, varargin)
 
-    if isa(obj, 'RobotInternalRepresentation')
-        obs_cat = obj.htm.DW.observed_categories;
-    elseif isa(obj, 'PerceivedEnvironment')
+    if isa(obj, 'PerceivedEnvironment')
         obs_cat = obj.DW.observed_categories;
     elseif isa(obj, 'HeadTurningModulationKS')
         obs_cat = obj.DW.observed_categories;
     elseif isa(obj, 'DynamicWeighting')
         obs_cat = obj.observed_categories;
+    else
+        obs_cat = obj.htm.DW.observed_categories;
     end
 
     if isempty(obs_cat)
-    	request = false;
+        request = false;
         return;
     end
     
     if nargin == 2
         field = 'all';
     end
+
+    if nargin == 4 && strcmp(varargin{1}, 'Object')
+        idx = getObject(obj, idx, 'audiovisual_category');
+    elseif nargin == 3 && strcmp(field, 'Object')
+        idx = getObject(obj, idx, 'audiovisual_category');
+        field = 'all';
+    end
+
 
     % if strcmp(field, 'all')
     %     idx = 1:numel(objects);
@@ -43,10 +51,8 @@ function request = getCategory (obj, idx, field)
 
     for iField = 1:numel(field)
         fname = char(field(iField));
-        request.(fname) = arrayfun(@(x) obs_cat{x}.(field{iField}), idx, 'UniformOutput', false);
-        % request{iField} = arrayfun(@(x) obs_cat{x}.(field{iField}), idx, 'UniformOutput', false);
+        request.(fname) = cell2mat(arrayfun(@(x) obs_cat{x}.(field{iField}), idx, 'UniformOutput', false));
     end
-    % request = request{:};
 
     if numel(field) == 1
         request = request.(fname);
