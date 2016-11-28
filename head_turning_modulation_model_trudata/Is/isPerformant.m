954ba3205a3829cf7ecfc1e0fa680240246aa5ba
bcl <cohen.lhyver@gmail.com>
2016-11-28 18:26:16 +0100

    MC

--- head_turning_modulation_model_trudata/Is/isPerformant.m
+++ head_turning_modulation_model_trudata/Is/isPerformant.m
@@ -1,8 +1,24 @@
-function bool = isPerformant (obj, idx)
-    perf = cell2mat(getCategory(obj, idx, 'perf'));
+function bool = isPerformant (obj, idx, varargin)
+% isPerformant function
+% This knowledge source aims at providing a representation of the internal representation of the environment
+% Author: Benjamin Cohen-Lhyver
+% Date: 26.09.16
+% Rev. 1.0
+
+	if isempty(idx)
+		bool = false;
+		return;
+	end
+    if nargin == 3 && strcmp(varargin{1}, 'Object')
+         idx = getObject(obj, idx, 'audiovisual_category');
+     end
+	
+    perf = getCategory(obj, idx, 'perf');
     
     if perf >= getInfo('q') && perf < 1
         bool = true;
+    elseif perf == 1 && getCategory(obj, idx, 'nb_inf') >= 6
+    	bool = true;
     else
         bool = false;
     end
