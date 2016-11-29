e50c9fc344c67a3d6e7fb7f296d892cf2a842848
bcl <cohen.lhyver@gmail.com>
2016-11-29 13:35:39 +0100

    MC

--- head_turning_modulation_model_trudata/Get/getLocalisationOutput.m
+++ head_turning_modulation_model_trudata/Get/getLocalisationOutput.m
@@ -2,13 +2,16 @@ function request = getLocalisationOutput (obj)
     if isa(obj, 'Blackboard')
         loc_KS = obj.getLastData('sourcesAzimuthsDistributionHypotheses').data;
     else
-        loc_KS = obj.blackboard.getLastData('sourcesAzimuthsDistributionHypotheses').data;
+        obj = obj.blackboard;
+        loc_KS = obj.getLastData('sourcesAzimuthsDistributionHypotheses').data;
     end
     if ~isempty(loc_KS)
+        head_position = obj.bbs.getCurrentHeadOrientation;
+
         hyp_loc = loc_KS.sourcesDistribution;
         [value, idx] = max(hyp_loc);
         % if value > 0.5
-            request = loc_KS.azimuths(idx);
+        request = mod(loc_KS.azimuths(idx)+head_position, 360);
         % else
         %     request = 0;
         % end
