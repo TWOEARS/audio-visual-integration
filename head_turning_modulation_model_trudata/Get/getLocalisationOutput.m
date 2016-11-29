8d7661e891ed1b43f4d0b5b302a583a95765dffd
bcl <cohen.lhyver@gmail.com>
2016-11-29 14:47:11 +0100

    MC

--- head_turning_modulation_model_trudata/Get/getLocalisationOutput.m
+++ head_turning_modulation_model_trudata/Get/getLocalisationOutput.m
@@ -13,7 +13,11 @@ function request = getLocalisationOutput (obj)
         hyp_loc = loc_KS.sourcesDistribution;
         [value, idx] = max(hyp_loc);
         % if value > 0.5
-        request = mod(loc_KS.azimuths(idx)+head_position, 360);
+        theta = loc_KS.azimuths(idx);
+        if request <= 195 || request >= 165
+            request = theta - 180;
+        end
+        request = mod(theta+head_position, 360);
         % else
         %     request = 0;
         % end
