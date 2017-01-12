6e649e2d2de8e3822514c0b505dd92fdff2f88ce
bcl <cohen.lhyver@gmail.com>
2017-01-12 16:32:32 +0100

    MC

--- head_turning_modulation_model_trudata/KnowledgeSources/VisualIdentityQRKS.m
+++ head_turning_modulation_model_trudata/KnowledgeSources/VisualIdentityQRKS.m
@@ -4,44 +4,61 @@
 % Rev. 2.0
 
 classdef VisualIdentityQRKS < AbstractKS
-    
-    properties (SetAccess = private)
-        robot;      % the robot environment interface
+
+% ======================== %
+% === PROPERTIES [BEG] === %
+% ======================== % 
+properties (SetAccess = private)
+    robot;      % the robot environment interface
+end
+% ======================== %
+% === PROPERTIES [END] === %
+% ======================== %
+
+% ===================== %
+% === METHODS [BEG] === %
+% ===================== %
+methods
+function obj = VisualIdentityQRKS(robot)
+    obj = obj@AbstractKS(); 
+    % initialize class members
+    obj.robot=robot;
+    % run continuously
+    obj.invocationMaxFrequency_Hz=inf;
+end
+
+
+%% execute functionality
+function [b, wait] = canExecute( obj )
+    % self-explanatory
+    b = true;
+    wait = false;
+end
+
+function execute (obj)
+    visual_vec = zeros(getInfo('nb_visual_labels'), 1);
+
+    visual_labels = getInfo('visual_labels');
+    perceived_data = obj.robot.getVisualData();
+
+    tmp = find(strcmp(perceived_data, visual_labels));
+    if ~isempty(tmp)
+        visual_vec(tmp) = 1;
     end
 
-    methods
-        function obj = VisualIdentityQRKS(robot)
-            obj = obj@AbstractKS(); 
-            % initialize class members
-            obj.robot=robot;
-            % run continuously
-            obj.invocationMaxFrequency_Hz=inf;
-        end
-        
-        
-        %% execute functionality
-        function [b, wait] = canExecute( obj )
-            % self-explanatory
-            b = true;
-            wait = false;
-        end
-        
-        function execute (obj)
-            visual_vec = zeros(getInfo('nb_visual_labels'), 1);
-
-            visual_labels = getInfo('visual_labels');
-            perceived_data = obj.robot.getVisualData();
-
-            tmp = find(strcmp(perceived_data, visual_labels));
-            if ~isempty(tmp)
-                visual_vec(tmp) = 1;
-            end
-            % push the visual identity hypothesis to the blackboard
-            visualIdentityHypotheses = containers.Map(visual_labels,...
-                                                      visual_vec);
-            obj.blackboard.addData( 'visualIdentityHypotheses',...
-                visualIdentityHypotheses, false, obj.trigger.tmIdx);
-            notify(obj, 'KsFiredEvent');
-        end
-    end
+    keySet = {'visual_labels', 'visual_vec'};
+    valueSet = {visual_labels, visual_vec};
+    % push the visual identity hypothesis to the blackboard
+    visualIdentityHypotheses = containers.Map(keySet, valueSet);
+    obj.blackboard.addData('visualIdentityHypotheses', visualIdentityHypotheses,...
+                           false, obj.trigger.tmIdx);
+    notify(obj, 'KsFiredEvent');
+end
+% ===================== %
+% === METHODS [END] === % 
+% ===================== %
+end
+% =================== %
+% === CLASS [END] === % 
+% =================== %
 end
