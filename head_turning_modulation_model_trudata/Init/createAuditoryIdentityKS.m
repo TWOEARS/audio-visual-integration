f46e881c2f13c0b34f78b28fc82dfee0a7f467cf
bcl <cohen.lhyver@gmail.com>
2017-01-16 21:21:11 +0100

    MC

--- head_turning_modulation_model_trudata/Init/createAuditoryIdentityKS.m
+++ head_turning_modulation_model_trudata/Init/createAuditoryIdentityKS.m
@@ -1,9 +1,13 @@
 function KS = createAuditoryIdentityKS (bbs, models)
 
+	global ROBOT_PLATFORM;
 	% folder = '/home/twoears/AuditoryModel/TwoEars-1.2/examples/identification_jido/Training.2016.01.31.21.11.16.074';
 	% folder = '/home/twoears/AuditoryModel/TwoEars-1.2/audio-visual-integration/head_turning_modulation_model_trudata/Dataset/mc2_models_dataset_1';
-
-    folder = '/home/bcl/AuditoryModel/TwoEars-1.2/database-internal/learned_models/IdentityKS/mc3_fc3_segmented_nsGroundtruth_models_dataset_1/x';
+	if strcmp(ROBOT_PLATFORM, 'ODI')
+    	folder = '/home/bcl/AuditoryModel/TwoEars-1.2/database-internal/learned_models/IdentityKS/mc3_fc3_segmented_nsGroundtruth_models_dataset_1/x';
+    elseif strcmp(ROBOT_PLATFORM, 'JIDO')
+    	folder = '/home/tforgue/mystuff/work/laas/twoears/database-internal/learned_models/IdentityKS/mc3_fc3_segmented_nsGroundtruth_models_dataset_1';
+    end
 	% folder = '/home/twoears/AuditoryModel/TwoEars-1.2/twoears-demos/identification_distractor_NIGENS/models.0db';
 	KS = arrayfun(@(x) bbs.createKS('IdentityKS', {models{x}, folder, false}),...
 				  1:numel(models),...
