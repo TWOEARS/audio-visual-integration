22f4143ca22099571d71e709eef7331edb32dcd4
bcl <cohen.lhyver@gmail.com>
2017-01-17 08:25:39 +0100

    MC

--- head_turning_modulation_model_trudata/Is/isPerformant.m
+++ head_turning_modulation_model_trudata/Is/isPerformant.m
@@ -15,9 +15,9 @@ function bool = isPerformant (obj, idx, varargin)
 	
     perf = getCategory(obj, idx, 'perf');
     
-    if perf >= getInfo('q') && perf < 1 && getCategory(obj, idx, 'nb_inf') >= 20
+    if perf >= getInfo('q') && perf < 1 && getCategory(obj, idx, 'nb_inf') >= 10
         bool = true;
-    elseif perf == 1 && getCategory(obj, idx, 'nb_inf') >= 20
+    elseif perf == 1 && getCategory(obj, idx, 'nb_inf') >= 10
     	bool = true;
     else
         bool = false;
