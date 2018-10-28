29e9ec056a95dddd530d5ad8ad02e8b6515104ff
bcl <cohen.lhyver@gmail.com>
2018-10-28 10:05:15 +0100

    Minor Change

--- head_turning_modulation_model_simdata/Tools/decreaseObjectWeight.m
+++ head_turning_modulation_model_simdata/Tools/decreaseObjectWeight.m
@@ -1,6 +1,9 @@
 function decreaseObjectWeight (htm, iObj)
 
 	obj = getObject(htm, iObj);
+	if obj.weight_hist(end) >= 0
+		obj.tsteps = 1;
+	end
 	obj.weight = 1/(1+0.01*exp(2*obj.tsteps)) - 1;
-
+	obj.weight_hist(end+1) = obj.weight;
 end
